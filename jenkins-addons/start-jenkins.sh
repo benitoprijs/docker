38263ca64a1a0c926a099ac5f27c1633c211dd1c
Rick Peters <rick.peters@me.com>
2014-11-21 15:30:40 +0100

    added jenkins proxy settings

--- jenkins-addons/start-jenkins.sh
+++ jenkins-addons/start-jenkins.sh
@@ -7,7 +7,15 @@ if [ $UID -ne 0 ]; then
 	exit 1
 fi
 
-echo Starting xvfb on DISPLAY $DISPLAY
-/usr/bin/Xvfb $DISPLAY -screen 0 1366x768x24 -ac&
+# modify config and give rights to the LDAP group in environment
+# variable LDAPGROUP
+if [ "$LDAPGROUP" = "" ]; then
+	echo 'LDAPGROUP not set in environment' >&2
+	exit 1
+fi
+
+cd /
+cd $JENKINS_HOME
+sed -i "s/{{groupname}}/$LDAPGROUP/g" ./config.xml
 
 su jenkins -c /usr/local/bin/jenkins.sh
--- jenkins-addons/strt_container.sh
+++ jenkins-addons/strt_container.sh
@@ -1,1 +1,10 @@
-sudo docker run -d -p 8083:8080 -v /var/appdata/jenkins_home_rick/jobs:/var/jenkins_home/jobs --name jenkins-addons kadaster/jenkins-addons
+#!/bin/bash
+
+# start de jenkins-addons container (met de kadaster plugins)
+# mount de container jenkins jobs directory aan de persistent jobs directory
+# mount de container jenkins log directory aan de host /var/log/jenkins directory (moet rechten hebben
+# voor uid 1000 (jenkins in de container)
+# plaats jenkins op poort 8083 op de host
+#
+
+sudo docker run -d -p 8083:8080 -e LDAPGROUP=jap-ont-dep -v /var/appdata/jenkins_home_rick/jobs:/var/jenkins_home/jobs -v /var/log/jenkins:/var/jenkins_home/log --name jenkins-addons kadaster/jenkins-addons
