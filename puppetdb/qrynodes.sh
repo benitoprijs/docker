efae8263a237ae727e03dca377e95336b828479b
Benito Prijs <prijsb@sou310.so.kadaster.nl>
2015-02-17 19:49:53 +0100

    orrected some typos and weird ^M characters

--- puppetdb/qrynodes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl --silent -G 'http://provisioning-plp-db.bhr.kadaster.nl:8080/v3/nodes' --data-urlencode 'query=["and",
-["=", ["fact", "werkomgeving"], "fto"],
-["=", ["fact", "customer"], "klic"],
-["~", ["fact", "application"], "klc-.*"]]'
-
